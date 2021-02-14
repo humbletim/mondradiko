8f5528bbc29d9813c4eb07a26c690e6d2d6c9589
humbletim <humbletim@gmail.com>
2021-02-14 02:38:21 -0500

    refactored vcpkg deps

--- cmake/msvc-disable-warnings.cmake
+++ cmake/msvc-disable-warnings.cmake
@@ -1,41 +1,45 @@
 set(MSVC_DISABLED_WARNINGS_LIST
-  C4061
-  C4099
-  C4100
-  C4189
-  C4191
-  C4201
-  C4242
-  C4244 #conversion from 'double' to 'T', possible loss of data
-  C4245
-  C4265
-  C4267
-  C4305
-  C4365
-  C4458
-  C4464 # relative include path contains '..'
-  C4514 #unreferenced inline function has been removed
-  C4582
-  C4583
-  C4623
-  C4625
-  C4626
-  C4668
-  C4702
-  C4710 # function '<x>' not inlined
-  C4711 # function <x> selected for inline expansion
-  C4774
-  C4820
-  C4866
-  C4868
-  C5026
-  C5027
-  C5039
-  C5045
-  C5204
-  C5219
+  C4061 # enumerator '...' in switch of enum '...' is not explicitly handled by a case label
+  C4099 # '...': type name first seen using '...' now seen using '...'
+  C4100 # '...': unreferenced formal parameter
+  C4189 # '...': local variable is initialized but not referenced
+  C4191 # '...': unsafe conversion from '...' to '...'
+  C4201 # nonstandard extension used: nameless struct/union
+  C4242 # '...': conversion from '...' to '...', possible loss of data
+  C4244 # '...': conversion from '...' to '...', possible loss of data
+  C4245 # '...': conversion from '...' to '...', signed/unsigned mismatch
+  C4265 # '...': class has virtual functions, but its non-trivial destructor is not virtual; instances of this class may not be destructed correctly
+  C4267 # '...': conversion from '...' to '...', possible loss of data
+  C4305 # '...': truncation from '...' to '...'
+  C4365 # '...': conversion from '...' to '...', signed/unsigned mismatch
+  C4458 # declaration of '...' hides class member
+  C4464 # relative include path contains '...'
+  C4514 # '...': unreferenced inline function has been removed
+  C4582 # '...': constructor is not implicitly called
+  C4583 # '...': destructor is not implicitly called
+  C4623 # '...': default constructor was implicitly defined as deleted
+  C4625 # '...': copy constructor was implicitly defined as deleted
+  C4626 # '...': assignment operator was implicitly defined as deleted
+  C4668 # '...' is not defined as a preprocessor macro, replacing with '...' for '...'
+  C4702 # unreachable code
+  C4710 # '...': function not inlined
+  C4710 # '...'::`2'...'::`2'...': function not inlined
+  C4711 # function '...' selected for automatic inline expansion
+  C4774 # '...' : format string expected in argument 3 is not a string literal
+  C4820 # '...': '...' bytes padding added after data member '...'
+  C4866 # compiler may not enforce left-to-right evaluation order for call to '...'
+  C4868 # compiler may not enforce left-to-right evaluation order in braced initializer list
+  C5026 # '...': move constructor was implicitly defined as deleted
+  C5027 # '...': move assignment operator was implicitly defined as deleted
+  C5039 # '...': pointer or reference to potentially throwing function passed to '...' function under -EHc. Undefined behavior may occur if this function throws an exception.
+  C5045 # Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
+  C5204 # '...': class has virtual functions, but its trivial destructor is not virtual; instances of objects derived from this class may not be destructed correctly
+  C5219 # implicit conversion from '...' to '...', possible loss of data
 )
-message(WARNING "FIXME: DISABLING LOTS OF C++ WARNINGS IN ORDER TO TROUBLESHOOT GHA CI... UNDO! ${MSVC_DISABLED_WARNINGS_LIST}")
-string(REPLACE "C" " -wd" MSVC_DISABLED_WARNINGS_STR ${MSVC_DISABLED_WARNINGS_LIST})
-set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} ${MSVC_DISABLED_WARNINGS_STR}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MSVC_DISABLED_WARNINGS_STR}")
+LIST(JOIN MSVC_DISABLED_WARNINGS_LIST " " MSVC_MSG )
+if (MSVC_DISABLE_KNOWN_WARNINGS)
+  message(STATUS "FIXME: DISABLING LOTS OF C++ WARNINGS IN ORDER TO TROUBLESHOOT GHA CI\n ${MSVC_MSG}")
+  string(REPLACE "C" " -wd" MSVC_DISABLED_WARNINGS_STR ${MSVC_DISABLED_WARNINGS_LIST})
+  set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} ${MSVC_DISABLED_WARNINGS_STR}")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MSVC_DISABLED_WARNINGS_STR}")
+endif()
--- cmake/optional-vcpkg-setup.cmake
+++ cmake/optional-vcpkg-setup.cmake
@@ -4,30 +4,72 @@
 # NOTE: this file needs to be included _before_ the first project() statement
 #   in order to properly bootstrap the CMAKE_TOOLCHAIN_FILE
 
+option(USE_VCPKG "Enable automated VCPKG support for installing dependencies" $ENV{USE_VCPKG})
+
 macro(enable_vcpkg_support)
   # enable vcpkg manifest mode
-  set(ENV{VCPKG_FEATURE_FLAGS} manifests)
+  #set(VCPKG_FEATURE_FLAGS manifests)
 
   # make sure x64 is used on windows (vcpkg otherwise defaults to x86-windows)
   if (WIN32)
-    set(ENV{VCPKG_DEFAULT_TRIPLET} "x64-windows")
+    set(VCPKG_DEFAULT_TRIPLET "x64-windows")
+  endif()
+  if (UNIX)
+    set(VCPKG_DEFAULT_TRIPLET "x64-linux")
   endif()
 
   # enable use of our custom triplet overlays
-  set(ENV{VCPKG_OVERLAY_TRIPLETS} ${CMAKE_CURRENT_LIST_DIR}/vcpkg-triplets)
-
+  #set(ENV{VCPKG_OVERLAY_TRIPLETS} ${CMAKE_CURRENT_LIST_DIR}/vcpkg-triplets)
+  set(VCPKG_OVERLAY_TRIPLETS ${CMAKE_SOURCE_DIR}/cmake/vcpkg-triplets)
   # enable use of our custom dependency portfiles
-  set(ENV{VCPKG_OVERLAY_PORTS} ${CMAKE_CURRENT_LIST_DIR}/vcpkg-ports)
+  set(VCPKG_OVERLAY_PORTS ${CMAKE_SOURCE_DIR}/cmake/vcpkg-ports)
+
+  set(_VCPKG_INSTALLED_DIR ${CMAKE_BINARY_DIR}/vcpkg_installed)
+
+  if(WIN32)
+    set(VCPKG_EXEC ${CMAKE_SOURCE_DIR}/vcpkg/vcpkg.exe)
+    set(VCPKG_BOOTSTRAP ${CMAKE_SOURCE_DIR}/vcpkg/bootstrap-vcpkg.bat)
+  else()
+    set(VCPKG_EXEC ${CMAKE_SOURCE_DIR}/vcpkg/vcpkg)
+    set(VCPKG_BOOTSTRAP ${CMAKE_SOURCE_DIR}/vcpkg/bootstrap-vcpkg.sh)
+  endif()
+
+  if(NOT EXISTS ${VCPKG_EXEC})
+    message("bootstrapping vcpkg in ${CMAKE_SOURCE_DIR}/vcpkg")
+    execute_process(COMMAND ${VCPKG_BOOTSTRAP} WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/vcpkg)
+  endif()
+
+  if(NOT EXISTS ${VCPKG_EXEC})
+    message(FATAL_ERROR "***** FATAL ERROR: Could not bootstrap vcpkg *****")
+  endif()
+  
+  if (BUILD_VULKAN_VALIDATION)
+    set(VULKAN_VALIDATION_DEP vulkan-validation)
+    message(">> To enable in-tree vulkan validation layers:\n\tset VK_LAYER_PATH=${_VCPKG_INSTALLED_DIR}/${VCPKG_DEFAULT_TRIPLET}/bin\n\t(before launching mondradiko-client.exe)")
+  else()
+    set(VULKAN_VALIDATION_DEP "")
+  endif()
+  message(STATUS "running vcpkg --x-install-root=${_VCPKG_INSTALLED_DIR} --overlay-ports=${VCPKG_OVERLAY_PORTS} --overlay-triplets=${CMAKE_OVERLAY_TRIPLETS} --triplet=${VCPKG_DEFAULT_TRIPLET}")
+  execute_process(
+    COMMAND ${VCPKG_EXEC}
+      --x-install-root=${_VCPKG_INSTALLED_DIR}
+      --overlay-ports=${VCPKG_OVERLAY_PORTS}
+      --overlay-triplets=${VCPKG_OVERLAY_TRIPLETS}
+      --triplet=${VCPKG_DEFAULT_TRIPLET}
+      install mondradiko-deps ${VULKAN_VALIDATION_DEP}
+  )
 
   # bootstrap the vcpkg toolchain file
   set(CMAKE_TOOLCHAIN_FILE ${CMAKE_SOURCE_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake CACHE STRING "")
 
-  message(STATUS "***** VCPKG MODE ENABLED *****")
-endmacro()
+  message(STATUS "VCPKG MODE ENABLED =========================================")
+  message(STATUS "     overlay-ports: ${CMAKE_SOURCE_DIR}/cmake/vcpkg-ports")
+  message(STATUS "  overlay-triplets: ${CMAKE_SOURCE_DIR}/cmake/vcpkg-triplets")
+  message(STATUS "      installed to: ${CMAKE_BINARY_DIR}/vcpkg_installed")
+  message(STATUS "============================================================")
 
-if (DEFINED ENV{USE_VCPKG} AND NOT DEFINED USE_VCPKG)
-  set(USE_VCPKG $ENV{USE_VCPKG})
-endif()
+  #include(${CMAKE_TOOLCHAIN_FILE})
+endmacro()
 
 if (USE_VCPKG)
   enable_vcpkg_support()
